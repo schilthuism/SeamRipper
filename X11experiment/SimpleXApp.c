f74486ae2c48a1efea8002e3a74efe30db96f730
Mike Levens <mlevens@contractor.ea.com>
2015-03-27 17:25:06 -0700

    now it does sine waves

--- X11experiment/SimpleXApp.c
+++ X11experiment/SimpleXApp.c
@@ -1,5 +1,39 @@
 #include <X11/Xlib.h>
 #include <unistd.h>
+#include <math.h>
+
+
+void drawWave( float wave[], int wavelen, Display *d, Window w, GC gc, 
+                int xoffset, int yoffset, int height, int width)
+{
+  int i, samp;
+  for (i=0; i<width; i++)
+    {
+       float sampmax = 0;   // for each column in the wave image, we're going to
+       float sampmin = 0;   // iterate over all the samples represented by that
+       int firstsamp = (i * wavelen) / width;                        // column.
+       int lastsamp = (((i + 1) * wavelen) / width) - 1;
+
+       for (samp=firstsamp; samp <= lastsamp; samp++)
+        { 
+          if (wave[samp] > sampmax)
+            sampmax = wave[samp];
+          if (wave[samp] < sampmin)
+            sampmin = wave[samp];
+        }
+
+        int x, ymid, y1, y2;
+        x = xoffset + i;
+        ymid = yoffset + (height/2);
+        y1 = ymid + (sampmax * height / 2);
+        y2 = ymid + (sampmin * height / 2);
+
+      XDrawLine(d, w, gc, x, y1, x, y2);
+    }
+  XFlush(d);
+}
+
+
 
 main()
 {
@@ -37,8 +71,20 @@ main()
   XFillRectangle(d, w, white, 50, 50, 200, 200);
   XFlush(d);
 
-  sleep(10); // OBSERVE THE BEAUTIFUL SQUARE BEFORE YOU
+  //////////HOLD ONTO YOUR HAT SHIT IS ABOUT TO GET BONKERS//////////
 
+  float davethesinewave[500];
+  int k;
+  for (k=0;k<500;k++)
+    {
+      davethesinewave[k] = sin(k);
+    }
+
+  drawWave( davethesinewave, 500, d, w, black, 
+               50, 50, 200, 200 );
+
+  sleep(10); // OBSERVE THE BEAUTIFUL WAVE BEFORE YOU
   
   return 0;
 }
+
